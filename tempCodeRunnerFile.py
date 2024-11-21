b91c8f7f3b510507336325050aa3167ddaaca409
Ankit Kuniyal <ankitkuniyal520@gmail.com>
2024-11-21 23:22:28 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-username = request.form['username']
-        email = request.form['email']
-        password = generate_password_hash(request.form['password'])
-        role = request.form['role']
-        desc = request.form.get('desc', '')  #
